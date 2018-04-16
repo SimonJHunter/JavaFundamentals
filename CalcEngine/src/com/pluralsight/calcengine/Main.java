f6dcb824baa63816b04773324913e2e2b2db6f07
Simon Hunter <33091112+Simon-Hunter@users.noreply.github.com>
2018-04-16 17:01:42 +0100

    Overloaded

--- CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -3,23 +3,33 @@ package com.pluralsight.calcengine;
 public class Main {
     public static void main(String[] args) {
         MathEquation [] equations = new MathEquation[4];
-        equations[0] = create(100.0d, 50.0d, 'd');
-        equations[1] = create(25.0d, 92.0d, 'a');
-        equations[2] = create(225.0d, 17.0d, 's');
-        equations[3] = create(11.0d, 3.0d, 'm');
+        equations[0] = new MathEquation('d',  100.0d, 50.0d);
+        equations[1] = new MathEquation('a', 25.0d, 92.0d);
+        equations[2] = new MathEquation('s', 225.0d, 17.0d);
+        equations[3] = new MathEquation('m', 11.0d, 3.0d);
 
         for (MathEquation equation: equations) {
             equation.execute();
-            System.out.println("result = " + equation.result);
+            System.out.println("result = " + equation.getResult());
         }
-    }
 
-    public static MathEquation create (double leftVal, double rightVal, char opCode) {
-        MathEquation equation = new MathEquation();
-        equation.leftVal = leftVal;
-        equation.rightVal = rightVal;
-        equation.opCode = opCode;
+        System.out.println("\n Using Overloads \n");
 
-        return equation;
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+        int leftInt = 9;
+        int rightInt = 4;
+        MathEquation equationOverload = new MathEquation('d');
+
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.println("result = " + equationOverload.getResult());
+
+        equationOverload.execute(leftInt, rightInt);
+        System.out.println("result = " + equationOverload.getResult());
+
+        equationOverload.execute((double)leftInt, rightInt);
+        System.out.println("result = " + equationOverload.getResult());
+
+
     }
 }
